fe11b1af15e7a197039abddcf71e0e1920316f01
Sam Scott <samueltscott@gmail.com>
2023-06-11 21:24:31 +0100

    Add support for multiple worlds and fix AssemblyInfo

--- HeadlessWorldSaveProtections/HeadlessWorldSaveProtections.cs
+++ HeadlessWorldSaveProtections/HeadlessWorldSaveProtections.cs
@@ -5,64 +5,82 @@ using BaseX;
 using CloudX.Shared;
 using System.Reflection;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace HeadlessWorldSaveProtections
 {
-    public class HeadlessWorldSaveProtections : NeosMod
-    {
-        public override string Name => "HeadlessWorldSaveProtections";
-        public override string Author => "Nytra";
-        public override string Version => "1.0.0";
-        public override string Link => "https://github.com/Nytra/HeadlessWorldSaveProtections";
-        public override void OnEngineInit()
-        {
-            Harmony harmony = new Harmony("owo.Nytra.HeadlessWorldSaveProtections");
+	public class HeadlessWorldSaveProtections : NeosMod
+	{
+		public override string Name => "HeadlessWorldSaveProtections";
+		public override string Author => "Nytra";
+		public override string Version => "1.0.0";
+		public override string Link => "https://github.com/Nytra/HeadlessWorldSaveProtections";
+		public override void OnEngineInit()
+		{
+			Harmony harmony = new Harmony("owo.Nytra.HeadlessWorldSaveProtections");
 
-            Type neosHeadless = AccessTools.TypeByName("NeosHeadless.Program");
-            if (neosHeadless != null)
-            {
-                harmony.PatchAll();
+			Type neosHeadless = AccessTools.TypeByName("NeosHeadless.Program");
+			if (neosHeadless != null)
+			{
+				harmony.PatchAll();
 
-                MethodInfo logOriginal = AccessTools.DeclaredMethod(typeof(UniLog), "Log", new Type[] { typeof(string), typeof(bool) });
-                MethodInfo logPostfix = AccessTools.DeclaredMethod(typeof(UniLog_Log_Patch), nameof(UniLog_Log_Patch.Postfix));
-                harmony.Patch(logOriginal, postfix: new HarmonyMethod(logPostfix));
-            }
-        }
+				MethodInfo logOriginal = AccessTools.DeclaredMethod(typeof(UniLog), "Log", new Type[] { typeof(string), typeof(bool) });
+				MethodInfo logPostfix = AccessTools.DeclaredMethod(typeof(UniLog_Log_Patch), nameof(UniLog_Log_Patch.Postfix));
+				harmony.Patch(logOriginal, postfix: new HarmonyMethod(logPostfix));
+			}
+		}
 
-        private static World currentlySavingWorld = null;
-        private static SessionAccessLevel prevSessionAccessLevel;
-        private static bool prevAwayKickEnabled;
+		class WorldInfo
+		{
+			public SessionAccessLevel prevAccessLevel;
+			public bool prevAwayKickEnabled;
+		}
 
-        [HarmonyPatch(typeof(Userspace), "SaveWorldTask")]
-        class Userspace_SaveWorldTask_Patch
-        {
-            public static bool Prefix(World world, FrooxEngine.Record record, RecordOwnerTransferer transferer)
-            {
-                Msg("World is saving. Setting session to private and disabling away kick.");
+		private static Dictionary<World, WorldInfo> worldInfoMap = new Dictionary<World, WorldInfo>();
 
-                currentlySavingWorld = world;
-                prevSessionAccessLevel = world.AccessLevel;
-                prevAwayKickEnabled = world.AwayKickEnabled;
+		[HarmonyPatch(typeof(Userspace), "SaveWorldTask")]
+		class Userspace_SaveWorldTask_Patch
+		{
+			public static bool Prefix(World world, FrooxEngine.Record record, RecordOwnerTransferer transferer)
+			{
+				Msg("World is saving. Setting session to private and disabling away kick.");
 
-                world.AccessLevel = SessionAccessLevel.Private;
-                world.AwayKickEnabled = false;
+				if (!worldInfoMap.ContainsKey(world))
+				{
+					worldInfoMap.Add(world, new WorldInfo());
+				}
 
-                return true;
-            }
-        }
+				worldInfoMap[world].prevAccessLevel = world.AccessLevel;
+				worldInfoMap[world].prevAwayKickEnabled = world.AwayKickEnabled;
 
-        class UniLog_Log_Patch
-        {
-            public static void Postfix(string message, bool stackTrace)
-            {
-                if (currentlySavingWorld != null && message.StartsWith("Finished save world: "))
-                {
-                    Msg("World finished saving. Restoring previous world config.");
-                    currentlySavingWorld.AccessLevel = prevSessionAccessLevel;
-                    currentlySavingWorld.AwayKickEnabled = prevAwayKickEnabled;
-                    currentlySavingWorld = null;
-                }
-            }
-        }
-    }
+				world.AccessLevel = SessionAccessLevel.Private;
+				world.AwayKickEnabled = false;
+
+				return true;
+			}
+		}
+
+		class UniLog_Log_Patch
+		{
+			public static void Postfix(string message, bool stackTrace)
+			{
+				if (message.StartsWith("World Saved! Name: ") && message.Contains(". RecordId: ") && message.Contains(". Local: ") && message.Contains(", Global: "))
+				{
+					foreach(World world in worldInfoMap.Keys.ToList())
+					{
+						FrooxEngine.Record r = world.CorrespondingRecord;
+
+						if (message.Contains($"World Saved! Name: {r.Name}. RecordId: {r.OwnerId}:{r.RecordId}"))
+						{
+							Msg($"World \"{world.Name}\" finished saving. Restoring session config.");
+							world.AccessLevel = worldInfoMap[world].prevAccessLevel;
+							world.AwayKickEnabled = worldInfoMap[world].prevAwayKickEnabled;
+							worldInfoMap.Remove(world);
+						}
+					}
+				}
+			}
+		}
+	}
 }
